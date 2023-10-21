1f6861adfa1b36879d2e03c770221bb4ca2f419c
NoCymer <61873313+NoCymer@users.noreply.github.com>
2023-10-21 14:53:36 +0200

    Fixing pom & adding deps

--- src/admin-app/src/main/java/com/volleyball/club/Main.java
+++ src/admin-app/src/main/java/com/volleyball/club/Main.java
@@ -1,7 +1,14 @@
 package com.volleyball.club;
+import java.sql.*; 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try{
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo","root","root");  
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
