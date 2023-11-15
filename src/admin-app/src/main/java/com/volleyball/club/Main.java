8e251be682439be6d7f9dbd06a3c7ab063e7c5df
NoCymer <61873313+NoCymer@users.noreply.github.com>
2023-11-15 09:09:47 +0100

    fixing display on linux

--- src/admin-app/src/main/java/com/volleyball/club/Main.java
+++ src/admin-app/src/main/java/com/volleyball/club/Main.java
@@ -2,7 +2,11 @@ package com.volleyball.club;
 import java.sql.*;
 
 import javax.swing.JFrame;
-import javax.swing.plaf.DimensionUIResource; 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import java.awt.Dimension;
 
 public class Main extends JFrame{
     public static void main(String[] args) {
@@ -15,10 +19,13 @@ public class Main extends JFrame{
         }
         JFrame f = new JFrame();
 
-        f.setVisible(true);
-        f.setSize(new DimensionUIResource(1024, 512));
-        f.setResizable(false);
+        f.setSize(new Dimension(1024, 512));
+        JPanel p = new JPanel();
+        p.setMinimumSize(new Dimension(1024, 512));
+        f.add(p, BorderLayout.CENTER);
         f.setLocationRelativeTo(null);
+        f.setResizable(false);
+        f.setVisible(true);
     }
 
 }
