9ab60e954a0d6c7921178f7f7ad29c5c56385017
NoCymer <61873313+NoCymer@users.noreply.github.com>
2023-11-15 08:57:53 +0100

    changing to version 11

--- src/admin-app/src/main/java/com/volleyball/club/Main.java
+++ src/admin-app/src/main/java/com/volleyball/club/Main.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello");
         try{
             Class.forName("com.mysql.jdbc.Driver");  
             DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo","root","root");  
