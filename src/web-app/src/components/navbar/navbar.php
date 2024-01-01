4706b3642f7317b37fed05c31bb35b5748430672
NoCymer <61873313+NoCymer@users.noreply.github.com>
2024-01-01 19:53:48 +0100

    navbar menu animation

--- src/web-app/src/components/navbar/navbar.php
+++ src/web-app/src/components/navbar/navbar.php
@@ -22,6 +22,35 @@
             echo(
                 '
                 <ul class="navbar">
+                    <li class="navbar-menu-opener">
+                        <svg class="nav-menu-symbol open" width="420" height="420" viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                                <style>
+                                    .nav-menu-symbol {
+                                        --symbol-color: black;
+                                    }
+                                </style>
+                            </defs>
+                            <path stroke="var(--symbol-color)" d="M63.0022 210L357.002 210M63 333.5L357 333.5M63.0022 87L357.002 87" stroke-width="73" stroke-linecap="round"/>
+                        </svg>
+                        <svg class="arrow-left-symbol close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 245.5 418">
+                            <defs>
+                                <style>
+                                    .arrow-left-symbol {
+                                        --symbol-color: black;
+                                    }
+                                    .cls-1{
+                                        fill: none;
+                                        stroke: var(--symbol-color);
+                                        stroke-linecap: round;
+                                        stroke-linejoin: round;
+                                        stroke-width: 73px;
+                                    }
+                                </style>
+                            </defs>
+                            <polyline class="cls-1" points="209 381.5 36.5 209 209 36.5"/>
+                        </svg>
+                    </li>
                     <ul class="navbar-menu">
                         <li '.($this->active==NavbarEntry::accueil ? 'class="selected"' : '').'><a href="/">ACCUEIL</a></li>
                         <li '.($this->active==NavbarEntry::informations ? 'class="selected"' : '').'><a href="/informations">INFORMATIONS</a></li>
@@ -39,11 +68,6 @@
                         )
                         .'
                     </ul>
-                    <li class="navbar-menu-opener">
-                        <svg width="420" height="420" viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M63.0022 210L357.002 210M63 333.5L357 333.5M63.0022 87L357.002 87" stroke-width="73" stroke-linecap="round"/>
-                        </svg>
-                    </li>
                 </ul>
                 '
             );
