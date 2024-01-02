883f8a571be23cb246667e22d450825fceaf0a38
NoCymer <61873313+NoCymer@users.noreply.github.com>
2024-01-02 21:47:31 +0100

    format

--- src/web-app/miniCssExtractCleanupPlugin.js
+++ src/web-app/miniCssExtractCleanupPlugin.js
@@ -2,24 +2,24 @@ const path = require("path");
 const fs = require("fs");
 
 class MiniCssExtractCleanupPlugin {
-  constructor(listOfFilesToRemove) {
-    this.listOfFilesToRemove = listOfFilesToRemove;
-  }
+    constructor(listOfFilesToRemove) {
+        this.listOfFilesToRemove = listOfFilesToRemove;
+    }
 
-  apply(compiler) {
-    compiler.hooks.afterEmit.tap(
-      "MiniCssExtractCleanupPlugin",
-      (compilation) => {
-        this.listOfFilesToRemove.forEach((file) => {
-          const filePath = path.join(compilation.outputOptions.path, file);
+    apply(compiler) {
+        compiler.hooks.afterEmit.tap(
+            "MiniCssExtractCleanupPlugin",
+            (compilation) => {
+                this.listOfFilesToRemove.forEach((file) => {
+                    const filePath = path.join(compilation.outputOptions.path, file);
 
-          if (fs.existsSync(filePath)) {
-            fs.unlinkSync(filePath);
-          }
-        });
-      }
-    );
-  }
+                    if (fs.existsSync(filePath)) {
+                        fs.unlinkSync(filePath);
+                    }
+                });
+            }
+        );
+    }
 }
 
 module.exports = MiniCssExtractCleanupPlugin;
--- src/web-app/webpack.config.js
+++ src/web-app/webpack.config.js
@@ -1,102 +1,82 @@
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const MiniCssExtractCleanupPlugin = require('./miniCssExtractCleanupPlugin');
+const MiniCssExtractCleanupPlugin = require("./miniCssExtractCleanupPlugin");
 
 module.exports = {
     entry: {
-      style: './src/styles/index.scss',
-      "components/navbar/navbar": './src/components/navbar/navbar.js',
-      "components/carrousel/carrousel": './src/components/carrousel/carrousel.js'
+        style: "./src/styles/index.scss",
+        "components/navbar/navbar": "./src/components/navbar/navbar.js",
+        "components/carrousel/carrousel": "./src/components/carrousel/carrousel.js",
     },
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
     },
     stats: {
-      errorDetails: true
+        errorDetails: true,
     },
     resolve: {
-      alias: {
-        "@Styles": path.resolve(__dirname, "src/styles"),
-        "@Public": path.resolve(__dirname, "../../public")
-      }
+        alias: {
+            "@Styles": path.resolve(__dirname, "src/styles"),
+            "@Public": path.resolve(__dirname, "../../public"),
+        },
     },
     module: {
-      rules: [
-        {
-          test: /\.s?css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
+        rules: [
             {
-              loader: "css-loader",
-              options: {
-                url: false,
-              }
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                        },
+                    },
+                    "sass-loader",
+                ],
             },
-            'sass-loader'
-          ]
-        }
-      ],
+        ],
     },
     optimization: {
-      minimizer: [
-        new CssMinimizerPlugin(),
-      ]
+        minimizer: [new CssMinimizerPlugin()],
     },
     plugins: [
         new MiniCssExtractPlugin({
-          filename: "css/styles.css"
+            filename: "css/styles.css",
         }),
         new MiniCssExtractCleanupPlugin(["style.js"]),
         new CopyPlugin({
-            patterns: [
-                { from: "src/preload.js", to: "js"}
-            ],
+            patterns: [{ from: "src/preload.js", to: "js" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/index.php", to: "index.php"}
-            ],
+            patterns: [{ from: "src/index.php", to: "index.php" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/app-config.php", to: "app-config.php"}
-            ],
+            patterns: [{ from: "src/app-config.php", to: "app-config.php" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/template.php", to: "template.php"}
-            ],
+            patterns: [{ from: "src/template.php", to: "template.php" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/pages"}
-            ],
+            patterns: [{ from: "src/pages" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "../../public", to: "public"}
-            ],
+            patterns: [{ from: "../../public", to: "public" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/components", to: "components"}
-            ],
+            patterns: [{ from: "src/components", to: "components" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/error-pages", to: "error-pages"}
-            ],
+            patterns: [{ from: "src/error-pages", to: "error-pages" }],
         }),
         new CopyPlugin({
-            patterns: [
-                { from: "src/.htaccess"}
-            ],
+            patterns: [{ from: "src/.htaccess" }],
         }),
     ],
-    devtool:"source-map"
+    devtool: "source-map",
 };
