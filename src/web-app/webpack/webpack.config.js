eb911c42bf0b21bb78932bbeb5053b200c94e25e
NoCymer <61873313+NoCymer@users.noreply.github.com>
2024-01-05 20:07:49 +0100

    uglifier for prod & optimizing gif size

--- src/web-app/webpack/webpack.config.js
+++ src/web-app/webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const MiniCssExtractCleanupPlugin = require("./plugins/miniCssExtractCleanupPlugin");
 const glob = require("glob");
@@ -56,7 +57,12 @@ module.exports = {
         ],
     },
     optimization: {
-        minimizer: [new CssMinimizerPlugin()],
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+            })
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
